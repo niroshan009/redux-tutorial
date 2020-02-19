ca9d682b2113d01b785c0de16e534415c535071e
Thusitha Kularathna <ThusithaK@mitrai.local>
2020-02-19 10:33:00 +0530

    add stylying

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,12 +2,20 @@ import React from "react";
 
 function TodoItem(props) {
   return (
-    <h4
-      onClick={props.onClick}
-      style={{ textDecoration: props.completed ? "line-through" : "none" }}
-    >
-      {props.text}
-    </h4>
+    <div align="left">
+      <input
+        name="isCompleted"
+        type="checkbox"
+        checked={props.completed}
+        onChange={props.onClick}
+      />
+      <label
+        style={{ textDecoration: props.completed ? "line-through" : "none" }}
+      >
+        {props.text}
+      </label>
+      <br />
+    </div>
   );
 }
 export default TodoItem;
